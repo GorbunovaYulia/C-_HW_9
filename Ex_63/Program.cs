25c1042bc2e44e13f297f84f57ffff1b5dd8f8ce
yulia <yuliagorbunova@yahoo.com>
2023-02-18 16:15:54 +0500

    Ex_63

---
+++ Ex_63/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 63. Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 => 1, 2, 3, 4, 5
+*/
+
+int getFromUserDate(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int userNumber = getFromUserDate("Введите значение N");
+printRange(userNumber);
+
+void printRange(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    printRange(N-1);
+    Console.Write(N + ",");
+}
