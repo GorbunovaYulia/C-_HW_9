086c66edb6c14dcbc04581b8e6d0149fdf1130fa
yulia <yuliagorbunova@yahoo.com>
2023-02-18 17:41:35 +0500

    HW_66

---
+++ Ex_69/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+69. Напишите программу, которая на вход принимает числа А и В, и возводит число А 
+в целую степень В с помощью рекурсии.
+А = 3, В = 5 --- 243, 3 в степени 5
+*/
+
+int getFromUserDate(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int getStepen(int numberA, int numberB)
+{
+    if (numberB == 0)
+    {
+        return 1;
+    }
+    return numberA * getStepen(numberA, numberB - 1);
+}
+
+int userNumberA = getFromUserDate("Введите число A");
+int userNumberB = getFromUserDate("Введите число B");
+int result = getStepen(userNumberA, userNumberB);
+Console.WriteLine(result);
--- HW_66/Program.cs
+++ HW_66/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int getFromUserDate(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int getSum(int numberM, int numberN)
+{
+    if (numberM == numberN)
+    {
+        return numberM;
+    }
+    return numberM + getSum(numberM + 1, numberN);
+}
+
+int userNumberM = getFromUserDate("Введите число M");
+int userNumberN = getFromUserDate("Введите число N");
+int userNumberM1 = Math.Min(userNumberM, userNumberN);
+int userNumberN1 = Math.Max(userNumberM, userNumberN);
+int result = getSum(userNumberM1, userNumberN1);
+Console.WriteLine(result);
